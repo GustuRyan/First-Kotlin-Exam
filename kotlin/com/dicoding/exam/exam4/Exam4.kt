8797b85bab81442f709a7f34609f3ff603588210
gus ryan <rian.putra2003@gmail.com>
2024-09-25 22:39:10 +0800

    answer for main exam number four

---
+++ kotlin/com/dicoding/exam/exam4/Exam4.kt
@@ -0,0 +1,11 @@
+package com.dicoding.exam.exam4
+
+// TODO
+fun vehicle(): Map<String, String> {
+    //mengembalikan data map dengan keterkaitan nilai sebagai berikut
+    return mapOf(
+        "type" to "motorcycle",
+        "maxSpeed" to "230Km/s",
+        "maxTank" to "100Ltr"
+    )
+}
