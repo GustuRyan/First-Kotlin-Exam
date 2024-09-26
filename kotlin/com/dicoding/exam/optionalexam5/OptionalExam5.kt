7127d2d42e672c975c0ac849bad9a9f62376f3f9
gus ryan <rian.putra2003@gmail.com>
2024-09-26 15:16:24 +0800

    answer for optional exam number five

---
+++ kotlin/com/dicoding/exam/optionalexam5/OptionalExam5.kt
@@ -0,0 +1,6 @@
+package com.dicoding.exam.optionalexam5
+
+// TODO
+val concatString: (String, String) -> String = String::plus
+//menerima dua inputan String dengan tipe   Lambda kemudian mengembalikan nilai String
+//kemudian dilakukan penggabungan String dengan metode plus
